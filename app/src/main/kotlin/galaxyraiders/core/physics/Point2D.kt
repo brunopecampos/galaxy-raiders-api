64ac336e57b09d486c7a22699134626c336d1bbf
brunopecampos <brunopecampos@protonmail.com>
2022-07-26 16:23:23 -0300

    Add part 1 of EP.
    Implemented required functions in Vector2D and Point2D
    classes. Passed in all tests.

--- app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
@@ -2,12 +2,15 @@
 package galaxyraiders.core.physics
 
 data class Point2D(val x: Double, val y: Double) {
+
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    val newPoint: Point2D = Point2D(this.x + p.x, this.y + p.y)
+    return newPoint
   }
 
   operator fun plus(v: Vector2D): Point2D {
-    return INVALID_POINT
+    val newPoint: Point2D = Point2D(this.x + v.dx, this.y + v.dy)
+    return newPoint
   }
 
   override fun toString(): String {
@@ -15,26 +18,28 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   fun toVector(): Vector2D {
-    return INVALID_VECTOR
+    val newVector: Vector2D = Vector2D(this.x, this.y)
+    return newVector
   }
 
   fun impactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    val newVector: Vector2D = Vector2D(Math.abs(this.x - p.x), Math.abs(this.y - p.y))
+    return newVector
   }
 
   fun impactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return this.impactVector(p).unit 
   }
 
   fun contactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return this.impactVector(p).normal
   }
 
   fun contactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return this.contactVector(p).unit
   }
 
   fun distance(p: Point2D): Double {
-    return INVALID_DOUBLE
+    return Math.sqrt((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y))
   }
 }
--- app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
@@ -10,57 +10,78 @@ data class Vector2D(val dx: Double, val dy: Double) {
   }
 
   val magnitude: Double
-    get() = INVALID_DOUBLE
+    get() = truncate(Math.sqrt(dx*dx + dy*dy))
 
   val radiant: Double
-    get() = INVALID_DOUBLE
+    get() = getRadiantVal(dx, dy)
 
   val degree: Double
-    get() = INVALID_DOUBLE
+    get() =  Math.toDegrees(radiant)
 
   val unit: Vector2D
-    get() = INVALID_VECTOR
+    get() = Vector2D(dx / magnitude, dy / magnitude)
 
   val normal: Vector2D
-    get() = INVALID_VECTOR
+    get() = Vector2D(unit.dy, -unit.dx)
 
   operator fun times(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    val newVector: Vector2D = Vector2D(truncate(this.dx * scalar), truncate(this.dy * scalar))
+    return newVector
   }
 
   operator fun div(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    val newVector: Vector2D = Vector2D(truncate(this.dx / scalar), truncate(this.dy / scalar))
+    return newVector
   }
 
   operator fun times(v: Vector2D): Double {
-    return INVALID_DOUBLE
+    val value: Double = truncate(truncate(this.dx * v.dx + this.dy * v.dy))
+    return value
   }
 
   operator fun plus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    val newVector: Vector2D = Vector2D(truncate(this.dx + v.dx), truncate(this.dy + v.dy))
+    return newVector
   }
 
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    val newPoint: Point2D = Point2D(p.x + this.dx, p.y + this.dy)
+    return newPoint 
   }
 
   operator fun unaryMinus(): Vector2D {
-    return INVALID_VECTOR
+    val newVector: Vector2D = Vector2D(-this.dx, -this.dy)
+    return newVector
   }
 
   operator fun minus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    val newVector: Vector2D = Vector2D(this.dx - v.dx, this.dy - v.dy)
+    return newVector
   }
 
   fun scalarProject(target: Vector2D): Double {
-    return INVALID_DOUBLE
+      return (this * target) / target.magnitude
   }
 
   fun vectorProject(target: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    val newVector: Vector2D = ((this * target) / truncate(target.magnitude * target.magnitude)) * target
+    return newVector
   }
 }
 
+fun getRadiantVal(dx: Double, dy: Double): Double {
+    val arccos : Double = Math.atan(Math.abs(dy) / Math.abs(dx))
+    var result : Double = if (dx >= 0.0) arccos else Math.PI - arccos 
+    result = if (dy >= 0.0) result else - result
+    return result
+  }
+
+const val TRUNC: Double = 100.0
+fun truncate(value: Double): Double {
+  return Math.floor(value * TRUNC) / TRUNC
+}
+
 operator fun Double.times(v: Vector2D): Vector2D {
-  return INVALID_VECTOR
+  val newVector : Vector2D = Vector2D(truncate(v.dx * this), truncate(v.dy * this))
+  return newVector
 }
